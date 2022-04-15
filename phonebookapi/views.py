2b2ebe67b68efd97e44044298fe580fce8e70938
Guizaida <80250687+guizaida@users.noreply.github.com>
2022-04-15 18:03:46 +0800

    Update views.py

--- phonebookapi/views.py
+++ phonebookapi/views.py
@@ -12,34 +12,71 @@ logger = Setup_Logger('Main_Api_Log','MainApiLog.log')
 class PhonebookViewSet(viewsets.ModelViewSet):
     queryset = phonebook.objects.all()
     serializer_class = PhonebookSerializer
-    
+
+class errorcode:
+    errorcode= {
+        503:{
+            'code':503,
+            'msg':'請求異常'
+        },
+        1000:{
+            'code':1000,
+            'msg':'此帳號尚未註冊或密碼錯誤'
+        },
+        1001:{
+            'code':1001,
+            'msg':'帳號或密碼錯誤'
+        },
+        1002:{
+            'code':1002,
+            'msg':'用戶攔是必須的'
+        },
+        1003:{
+            'code':1003,
+            'msg':'用戶已存在'
+        },
+        1004:{
+            'code':1004,
+            'msg':'非法電話號碼'
+        },
+        1005:{
+            'code':1005,
+            'msg':'無此帳號或以刪除'
+        },
+        1006:{
+            'code':1006,
+            'msg': '密碼錯誤'
+        },
+        1007:{
+            'code':1007,
+            'msg': '未登入'
+        },
+    }
     
 class AuthView(APIView):
     #登入api
     def post(self, request, *args, **kwargs):
-        ret = {'code': 200, 'msg': '成功登入'}
+        ret = {'code': 201, 'msg': '成功登入'}
         try:
             user = request._request.POST.get('UserName')
             pwd = request._request.POST.get('Password')
             # 驗證此帳號是否已存在資料庫
             obj = models.phonebook.objects.filter(UserName=user, Password=pwd).first()
             if not obj:
-                ret['code'] = 1000
-                ret['msg'] = '此帳號尚未註冊或密碼錯誤'
+                ret = errorcode.errorcode[1000]
             else:
                 # 存在就創建或更新token
                 token = md5(user)
                 ret['token'] = token
                 models.UserToken.objects.update_or_create(defaults={'token': token}, user=obj)
         except Exception as e:
-            ret['code'] = 503
-            ret['msg'] = '請求異常'
+            ret = errorcode.errorcode[503]
             logger.info('Auth Error - {0}'.format(e))   
         return JsonResponse(ret)
     
 class updateView(APIView):
     def post(self, request, *args, **kwargs):
-        ret = {'code': 200, 'msg': '成功修改'}
+        ret = {'code': 201, 'msg': '成功修改'}
         try:
             user = request._request.POST.get('UserName')
             pwd = request._request.POST.get('Password')
@@ -48,20 +85,18 @@ class updateView(APIView):
             if pwd == pwdd:
                 obj = models.phonebook.objects.filter(UserName=user, Password=pwd)
                 if not obj:
-                    ret['code'] = 1001
-                    ret['msg'] = '帳號或密碼錯誤'
+                    ret = errorcode.errorcode[1001]
                 else:
                     obj.update(PhoneNumber=npnb)
         except Exception as e:
-            ret['code'] = 503
-            ret['msg'] = '請求異常'
+            ret = errorcode.errorcode[503]
             logger.info('update Error - {0}'.format(e))   
         return JsonResponse(ret)
     
     
 class registerView(APIView):
     def post(self, request, *args, **kwargs):
-        ret = {'code': 200, 'msg': '成功註冊'}
+        ret = {'code': 201, 'msg': '成功註冊'}
         try:
             user = request._request.POST.get('UserName')
             pwd = request._request.POST.get('Password')
@@ -73,25 +108,21 @@ class registerView(APIView):
                     models.phonebook.objects.update_or_create(UserName =user,Password = pwd,PhoneNumber = phnb)
                 else:
                     if not user:
-                        ret['code'] = 1002
-                        ret['msg'] = '用戶攔是必須的'
+                        ret = errorcode.errorcode[1002]
                     else:
-                        ret['code'] = 1003
-                        ret['msg'] = '用戶已存在'
+                        ret = errorcode.errorcode[1003]
             except:
-                ret['code'] = 1004
-                ret['msg'] = '非法電話號碼'
+                ret = errorcode.errorcode[1004]
                 logger.info('register Error - {0}'.format(e)) 
             # 驗證資料庫是否存在此用戶
             
         except Exception as e:
-            ret['code'] = 503
-            ret['msg'] = '請求異常'
+            ret = errorcode.errorcode[503]
         return JsonResponse(ret)
 
 class DeleteView(APIView):
     def post(self, request, *args, **kwargs):
-        ret = {'code': 200, 'msg': '成功刪除帳號'}
+        ret = {'code': 201, 'msg': '成功刪除帳號'}
         try:
             user = request._request.POST.get('UserName')
             pwd = request._request.POST.get('Password')
@@ -99,33 +130,27 @@ class DeleteView(APIView):
             if pwd == pwdd:
                 obj = models.phonebook.objects.filter(UserName =user,Password = pwd).first()
                 if not obj:
-                    ret['code'] = 1005
-                    ret['msg'] = '無此帳號或以刪除'
+                    ret = errorcode.errorcode[1005]
                 else:
                     models.phonebook.objects.filter(UserName =user).delete()
             else:
-                ret['code'] = 1006
-                ret['msg'] = '密碼錯誤'
+                ret = errorcode.errorcode[1006]
                 
         except Exception as e:
-            ret['code'] = 503
-            ret['msg'] = '請求異常'
+            ret = errorcode.errorcode[503]
             logger.info('Delete Error - {0}'.format(e)) 
         return JsonResponse(ret)
     
 class tkcheckView(APIView):
     def post(self, request, *args, **kwargs):
-        ret = {'code': 200, 'msg': '驗證成功'}
+        ret = {'code': 201, 'msg': '驗證成功'}
         try:
             tk = request._request.POST.get('Token')
-            print(tk)
             obj = models.UserToken.objects.filter(token = tk).first()
             if not obj:
-                ret['code'] = 1007
-                ret['msg'] = '未登入'
+                ret = errorcode.errorcode[1007]
         except Exception as e:
-            ret['code'] = 503
-            ret['msg'] = '請求異常'
+            ret = errorcode.errorcode[503]
             logger.info('tkcheck Error - {0}'.format(e)) 
         return JsonResponse(ret)
     
